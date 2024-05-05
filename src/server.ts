e575f66142ffc30e268f1b86aa5fa8990b831b20
JonahtanCF <jonathanf439@gmail.com>
2024-05-05 20:44:48 -0300

    server

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,23 @@
-import fastify from 'fastify';
-import fastifySwagger from '@fastify/swagger';
-import fastifySwaggerUi from '@fastify/swagger-ui';
-import fastifyCors from '@fastify/cors';
-import { serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider } from 'fastify-type-provider-zod';
-import { errorHandler } from './error-handler';
+import fastify from "fastify";
 
-// Import routes
-import { createEvent } from './routes/create-event';
-import { registerForEvent } from './routes/register-for-events';
-import { getEvent } from './routes/get-event';
-import { getAttendeeBadge } from './routes/get-attendee-badge';
-import { checkIn } from './routes/check-in';
-import { getEventAttendees } from './routes/get-event.attendees';
-// import { getAllEvent } from './routes/get-all-events'
-// import { getAllAttendees } from './routes/get-all-attendees'
-// import { getAllCheckIn } from './routes/get-all-checkIn'
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-events";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event.attendees";
+import { errorHandler } from "./error-handler";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-// Plugins
 app.register(fastifyCors, {
-  origin: '*', // Allow requests from all origins
-});
+  origin: '*',
+})
 
 app.register(fastifySwagger, {
   swagger: {
@@ -30,36 +25,28 @@ app.register(fastifySwagger, {
     produces: ['application/json'],
     info: {
       title: 'pass.in',
-      description: 'API specifications for the pass.in backend application.',
-      version: '1.0.0',
+      description: 'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
+      version: '1.0.0'
     },
   },
   transform: jsonSchemaTransform,
-});
+})
 
-app.register(fastifySwaggerUi, {
+app.register(fastifySwaggerUI, {
   routePrefix: '/docs',
-});
+})
 
-// Custom serializers and validators
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-// Register routes
-app.register(createEvent);
-// app.register(getAllEvent)
-app.register(registerForEvent);
-app.register(getEvent);
-app.register(getAttendeeBadge);
-app.register(checkIn);
-app.register(getEventAttendees);
-// app.register(getAllAttendees)
-// app.register(getAllCheckIn)
+app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
+app.register(checkIn)
+app.register(getEventAttendees)
 
-
-
-// Error handler
-app.setErrorHandler(errorHandler);
+app.setErrorHandler(errorHandler)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
